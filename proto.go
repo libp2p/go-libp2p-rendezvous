9135bf5c15986d9115929b5111ef28bacac3979a
gfanton <8671905+gfanton@users.noreply.github.com>
2022-11-08 17:10:26 +0100

    chore: disable golangci revive for now
    Signed-off-by: gfanton <8671905+gfanton@users.noreply.github.com>

--- proto.go
+++ proto.go
@@ -27,7 +27,7 @@ type RendezvousError struct {
 }
 
 func (e RendezvousError) Error() string {
-	return fmt.Sprintf("Rendezvous error: %s (%s)", e.Text, pb.Message_ResponseStatus(e.Status).String())
+	return fmt.Sprintf("Rendezvous error: %s (%s)", e.Text, e.Status.String())
 }
 
 func NewRegisterMessage(ns string, pi peer.AddrInfo, ttl int) *pb.Message {
